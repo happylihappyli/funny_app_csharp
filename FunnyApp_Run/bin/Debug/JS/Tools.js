630680ca8d15646e67d5dd766b02c6efdffea6ce
happyli <happyli.net@gmail.com>
2019-08-31 13:49:22 +0800

    u

--- FunnyApp_Run/bin/Debug/JS/Tools.js
+++ FunnyApp_Run/bin/Debug/JS/Tools.js
@@ -3,6 +3,10 @@ function show_tools(data){
     sys.Run_JS(data+".js");
 }
 
+function show_tools2(data){
+    sys.Run_JS_Out(data+".js");
+}
+
 function draw_test(data){
     sys.PictureBox_Draw_Ellipse("pic1",50,50,100,100,"blue",2);
 }
@@ -23,13 +27,14 @@ sys.Button_Init("b1_2","科学上网",200,50,100,30,"show_tools","代理");
 
 sys.Button_Init("b1_3","上传文件",350,50,100,30,"show_tools","上传文件");
 
-sys.Button_Init("b1_4","闹钟",500,50,100,30,"show_tools","闹钟");
+sys.Button_Init("b1_4","闹钟",500,50,100,30,"show_tools2","闹钟");
 sys.Button_Init("b1_5","聊天",650,50,100,30,"show_tools","聊天");
 
 sys.Button_Init("b2_1","计算器",50,150,100,30,"show_tools","计算器");
+sys.Button_Init("b2_2","加密收藏夹",200,150,100,30,"show_tools2","FunnyFav\\index");
 
-sys.Button_Init("b2_2","draw",200,150,100,30,"draw_test","");
-sys.Button_Init("b2_3","draw",350,150,100,30,"draw_test2","");
+sys.Button_Init("b2_3","draw",350,150,100,30,"draw_test","");
+sys.Button_Init("b2_4","draw",500,150,100,30,"draw_test2","");
 
 sys.PictureBox_Init("pic1",50,200,300,200);
 sys.PictureBox_Event("pic1","mouse_up");
